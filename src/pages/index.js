73c896582d23f00db3c06b09e90027311fc2d2c5
Minseong Kim <62862158+minstar599@users.noreply.github.com>
2020-12-30 01:49:02 +1000

    흐앙

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,43 @@ import '../../css/default.css'
 import '../../css/fonts.css'
 import '../../css/style.css'
 import '../../css/responsive.css'
+import '../../css/jstyle.css'
 
 export default function Home() {
   return (
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Hi Art Studio</title>
-
-		<link> rel="stylesheet" href="../../css/newStyle.css" </link> 
-
-
-		<link> rel="stylesheet" href="../../css/bootstrap.css" </link> 
-		<link> rel="stylesheet" href="vendors/fontawesome/css/all.min.css" </link> 
-		<link> rel="stylesheet" href="vendors/animate-css/animate.css" </link>
-		<link> rel="stylesheet" href="vendors/popup/magnific-popup.css" </link>
-
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
 		
-		<link> rel="stylesheet" href="../../css/default.css" </link>
-		<link> rel="stylesheet" href="../../css/fonts.css" </link>
-		<link> rel="stylesheet" href="../../css/style.css" </link>
-		<link> rel="stylesheet" href="../../css/responsive.css" </link> 
 	</head>,
 
 	<body>
 		<div id="header-wrapper">
-		<div id="header" class="container">
-			<div id="logo">
-				<h1><a href="#">Hi Art Studio</a></h1>
+			<div id="header" class="container">
+				<div id="logo">
+					<h1><a href="#">Hi Art Studio</a></h1>
+				</div>
 			</div>
-		</div>
 		</div>	
+		
+		<header id="header" class="header_area">
+			<div id="menu" class="main_menu">
+				<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+
+						<ul id="topmenu">
+							<li ><a href="#">Home</a></li> 
+							<li ><a href="#">About</a></li> 
+							<li ><a href="#">Portfolio</a></li>    
+							<li ><a href="#">FAQ</a></li>
+							<li ><a href="#">Contact us</a></li>  
+						</ul>
+
+					</div>
+				</nav>
+			</div>
+		</header>
+
 	</body>
   )
 }
