f8f091823281cd5d4cc9a7afca78b326f9ed65f2
Minseong Kim <62862158+minstar599@users.noreply.github.com>
2020-12-30 22:42:49 +1000

    조금 된다

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
 import React from "react"
-import '../../css/default.css'
-import '../../css/fonts.css'
-import '../../css/style.css'
-import '../../css/responsive.css'
+
 import '../../css/jstyle.css'
+import '../../css/bootstrap.css'
+
 
 export default function Home() {
   return (
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Hi Art Studio</title>
-		
 	</head>,
 
 	<body>
@@ -26,15 +24,29 @@ export default function Home() {
 			<div id="menu" class="main_menu">
 				<nav class="navbar navbar-expand-lg navbar-light">
 					<div class="container">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
 
-						<ul id="topmenu">
-							<li ><a href="#">Home</a></li> 
-							<li ><a href="#">About</a></li> 
-							<li ><a href="#">Portfolio</a></li>    
-							<li ><a href="#">FAQ</a></li>
-							<li ><a href="#">Contact us</a></li>  
-						</ul>
-
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto mr-auto active">
+								<li class="nav-item active"><a class="nav-link" href="#">Home</a></li> 
+								<li class="nav-item"><a class="nav-link" href="#">About</a></li> 
+								<li class="nav-item"><a class="nav-link" href="#">Portfolio</a></li>    
+								<li class="nav-item"><a class="nav-link" href="#">FAQ</a></li>
+								<li class="nav-item"><a class="nav-link" href="#">Contact us</a></li>   
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Makeup Lesson</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Blog Details</a></li>
+									</ul>
+								</li>
+		
+							</ul>
+						</div>
 					</div>
 				</nav>
 			</div>
