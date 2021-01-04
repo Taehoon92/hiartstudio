729198d217cb9f2eb98f76e907c545780c177fef
Taehoon92 <hoontae92@gmail.com>
2021-01-04 10:07:40 +1000

    Header 안에 Menu 넣고, Index 파일 Layout 으로 깔끔하게 정리

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,52 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import '../../css/newStyle.css'
+
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <header id="header" className="header_area">
+    <div id="menu" className="main_menu">
+      <div id="logo">
+        <Link to ="/">
+          <img src='../../logo.png' />
         </Link>
-      </h1>
-    </div>
+      </div>
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+
+                <div className="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <ul className="nav navbar-nav menu_nav ml-auto mr-auto active">
+                        <li className="nav-item submenu dropdown">
+                          <a href="/about/" className="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+                          <ul className="dropdown-menu">
+                            <li className="nav-item"><a className="nav-link" href="#">Teachers</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Studio</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Cirriciulum</a></li>
+                          </ul>
+                        </li>
+                        <li className="nav-item"><a className="nav-link" href="#">Portfolio</a></li>    
+                        <li className="nav-item"><a className="nav-link" href="#">FAQ</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Contact us</a></li>   
+                        <li className="nav-item submenu dropdown">
+                            <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Makeup Lesson</a>
+                            <ul className="dropdown-menu">
+                                <li className="nav-item"><a className="nav-link" href="#">Blog</a></li>
+                                <li className="nav-item"><a className="nav-link" href="#">Blog Details</a></li>
+                            </ul>
+                        </li>
+  
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>  
+
   </header>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import React from "react"
 import '../../css/newStyle.css'
+import Header from "../components/header"
+
 
 export default function About() {
   return (
-    <div>
+    <div id="page-wrapper">
+      <Header />
       <h1>About me</h1>
       <p>
         I’m good enough, I’m smart enough, and gosh darn it, people like me!
