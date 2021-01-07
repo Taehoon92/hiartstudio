d43c91f0227672854bf11ce0221535671b824f23
Minseong Kim <62862158+minstar599@users.noreply.github.com>
2021-01-08 00:48:53 +1000

    어바웃 페이지에 이미지 위에 텍스트 올리기 시도중

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,11 +6,20 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div id="page-wrapper">
+
+
       <Header />
-      <h1>About me</h1>
-      <p>
-        I’m good enough, I’m smart enough, and gosh darn it, people like me!
-      </p>
+
+
+      <div class="aboutimage">
+        <h1 class="text container">
+          About Us
+        </h1>
+      </div>
+
+      
+
+
     </div>
   )
 }
