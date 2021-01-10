56de02a33b44849db5a08c8dc90a81118c5e2898
Minseong Kim <62862158+minstar599@users.noreply.github.com>
2021-01-11 05:19:35 +1000

    minor changes

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -16,15 +16,17 @@ export default function Contactus() {
           <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/12804615_844351048999090_7366405912543968522_n.jpg?_nc_cat=109&ccb=2&_nc_sid=6e5ad9&_nc_ohc=D2wN4XsJWwgAX_tF7p5&_nc_ht=scontent-syd2-1.xx&oh=302c9917bb8833ddf790ab3dd403e33b&oe=602201FC" />
         </div>
 
+        <div className="blank-space"></div>
+
         <div className="container">
 
         
           <div className = "row">
-            <div className="contact-box col-md-4 ">
+            <div className="contact-box col">
                 <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/69996523_2115980488502800_2822794296686542848_n.jpg?_nc_cat=100&ccb=2&_nc_sid=8bfeb9&_nc_ohc=mXSCFdKIvT4AX_m-cgk&_nc_ht=scontent-syd2-1.xx&oh=8cedab965948fa65c4772944914b9a70&oe=601F7550" />
             </div>  
 
-            <div className="contact-box col-md-4">
+            <div className="contact-box col">
                 <h3>Phone</h3>
                 <p>Head-Teacher Lan Kim: (+61) 0405 068 004</p>
 
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -19,27 +19,36 @@ export default function Gallery() {
         <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/12804615_844351048999090_7366405912543968522_n.jpg?_nc_cat=109&ccb=2&_nc_sid=6e5ad9&_nc_ohc=D2wN4XsJWwgAX_tF7p5&_nc_ht=scontent-syd2-1.xx&oh=302c9917bb8833ddf790ab3dd403e33b&oe=602201FC" />
       </div>
 
-      <div className = "row">
-
-        <div className="gallery-box col-md-3 ">
-            <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/s960x960/128096892_3153678894732949_1979257469722480982_o.jpg?_nc_cat=109&ccb=2&_nc_sid=8024bb&_nc_ohc=AkPxAO233X0AX8i_zZp&_nc_ht=scontent-syd2-1.xx&tp=7&oh=51fd93e16d62baa3f9ec729e65d32552&oe=6021327B" className="gallery-image" />
-        </div>  
+      <div className="blank-space"></div>
 
 
-        <div className="gallery-box col-md-3">
-            <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/60281435_1918302531603931_998723434127032320_o.jpg?_nc_cat=107&ccb=2&_nc_sid=8024bb&_nc_ohc=SLNRkLRxrxkAX9WetJb&_nc_ht=scontent-syd2-1.xx&oh=b061e2b87921342431d70598731fce15&oe=601F8978" className="gallery-image" />
-        </div>  
+      <div className="container">
+
+      
+        <div className = "row">
+
+          <div className="gallery-box col-4">
+              <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/s960x960/128096892_3153678894732949_1979257469722480982_o.jpg?_nc_cat=109&ccb=2&_nc_sid=8024bb&_nc_ohc=AkPxAO233X0AX8i_zZp&_nc_ht=scontent-syd2-1.xx&tp=7&oh=51fd93e16d62baa3f9ec729e65d32552&oe=6021327B" className="gallery-image" />
+          </div>  
 
 
-        <div className="gallery-box col-md-3">
-            <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/60281435_1918302531603931_998723434127032320_o.jpg?_nc_cat=107&ccb=2&_nc_sid=8024bb&_nc_ohc=SLNRkLRxrxkAX9WetJb&_nc_ht=scontent-syd2-1.xx&oh=b061e2b87921342431d70598731fce15&oe=601F8978" className="gallery-image" />
-        </div>  
+          <div className="gallery-box col-4">
+              <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/60281435_1918302531603931_998723434127032320_o.jpg?_nc_cat=107&ccb=2&_nc_sid=8024bb&_nc_ohc=SLNRkLRxrxkAX9WetJb&_nc_ht=scontent-syd2-1.xx&oh=b061e2b87921342431d70598731fce15&oe=601F8978" className="gallery-image" />
+          </div>  
 
-        <div className="gallery-box col-md-3">
-            
-        </div>  
 
-        
+          <div className="gallery-box col-4">
+              <img src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/60281435_1918302531603931_998723434127032320_o.jpg?_nc_cat=107&ccb=2&_nc_sid=8024bb&_nc_ohc=SLNRkLRxrxkAX9WetJb&_nc_ht=scontent-syd2-1.xx&oh=b061e2b87921342431d70598731fce15&oe=601F8978" className="gallery-image" />
+          </div>  
+
+          <div className="gallery-box col-4">
+              
+          </div>  
+          
+
+          
+        </div>
+
       </div>
 
 
