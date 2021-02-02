bb31ec47b154544405943b90ac2cb97316be6058
Minseong Kim <62862158+minstar599@users.noreply.github.com>
2021-02-02 23:54:47 +1000

    posting index

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,56 @@ const IndexPage = () => (
 					<h2>Welcome to our website</h2>
 				</div>
 				<p>We are located at 5 Saville St. Eight Mile Plains QLD 4113</p>
+
+				<div className="container">
+					<table>
+						<thead>
+						<tr>
+							<th>No.</th>
+							<th>Title</th>
+							<th>Date</th>
+							<th>Views</th>
+						</tr>
+						</thead>
+						<tbody>
+						<tr>
+							<td>1</td>
+							<td>First Post.</td>
+							<td>2020-10-25</td>
+							<td>6</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Second Post.</td>
+							<td>2020-10-25</td>
+							<td>5</td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>Third Post.</td>
+							<td>2020-10-25</td>
+							<td>1</td>
+						</tr>
+						<tr>
+							<td>4</td>
+							<td>Fourth Post.</td>
+							<td>2020-10-25</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>5</td>
+							<td>Fifth Post.</td>
+							<td>2020-10-25</td>
+							<td>4</td>
+						</tr>
+						</tbody>
+					</table>
+
+					<button>
+						Write Post
+					</button>
+
+				</div>
 			
 			</div>
 		</div>
