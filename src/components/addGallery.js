b685cd879d0a524274cff95d3846b4b9bcaf8cfd
taehoon92 <hoontae92@gmail.com>
2021-02-17 16:28:43 +1000

    갤러리 loadmore 함수 테스트중

--- src/components/addGallery.js
+++ src/components/addGallery.js
@@ -3,5 +3,11 @@ import React, {useEffect} from "react"
 export default ({idx}) => {
 
     const htmlCode = ""
-    return <div dangerouslySetInnerHTML={{__html:htmlCode}}></div>
+    
+    console.log({idx});
+    
+    return (
+        <div dangerouslySetInnerHTML={{__html:htmlCode}}></div>
+
+    )
 }
